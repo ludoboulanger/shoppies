82c890fdcc4e1cd417afd8ab71208ad7ca2740f7
lboulanger <ludovic.boulanger@outlook.com>
2021-01-07 13:01:42 -0500

    Refactored the list creations

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,17 +3,80 @@ import './Home.css';
 import MovieList from '../MovieList/MovieList';
 import { fetchMovies } from '../../utils/MovieFetcher';
 import Searchbar from '../Search/Search';
+import { DragDropContext } from 'react-beautiful-dnd';
+import _ from 'lodash';
 
 export default function Home() {
-  const [availableMovies, setAvailableMovies] = useState([]);
+
+  const [movieLists, setMovieLists] = useState(
+   [
+     {
+       listId: "searchResults",
+       list: [],
+     },
+     {
+       listId: "votedMovies",
+       list: [],
+     },
+   ]
+  )
 
   const fetchData = async (title) => {
-    let movies = await fetchMovies(title);
-    console.log(movies);
-    setAvailableMovies(movies);
+    let newMovieLists = _.cloneDeep(movieLists)
+    let newSearchResults = await fetchMovies(title);
+    console.log(newSearchResults);
+
+    let prevSearchResults =  _.find(newMovieLists, list => list.listId === "searchResults")
+
+    if (prevSearchResults) {
+      prevSearchResults.list = newSearchResults;
+    }
+
+    setMovieLists(newMovieLists);
   };
 
-  return(
+  const getSearchResultsList = () => {
+    let movieList = _.find(movieLists, list => list.listId === "searchResults");
+
+    if (movieList) {
+      return movieList.list;
+    } else {
+      return [];
+    }
+  };
+
+  const getVotedMoviesList = () => {
+    let movieList = _.find(movieLists, list => list.listId === "votedMovies");
+
+    if (movieList) {
+      return movieList.list;
+    } else {
+      return [];
+    }
+  }
+
+  const handleDragEnd = (result) => {
+    console.log(result);
+    // if (result.destination) {
+    //   let newMovies = _.cloneDeep(availableMovies);
+    //   let [draggedMovie] = newMovies.splice(result.source.index, 1);
+
+    //   if (result.source)
+
+    //   if (result.destination.droppableId === "searchResults") {
+    //     newMovies.splice(result.destination.index, 0, draggedMovie);
+    //     setAvailableMovies(newMovies);
+    //   } else {
+    //     let newVotedMovies = _.cloneDeep(votedMovies);
+
+    //     newVotedMovies.splice(result.destination.index, 0, draggedMovie);
+    //     setAvailableMovies(newMovies);
+    //     setVotedMovies(newVotedMovies);
+    //   }
+    // }
+  };
+
+  return (
     <div className="main-grid">
       <div className="header">
         <h1 className="title">Shoppies</h1>
@@ -24,14 +87,27 @@ export default function Home() {
         <Searchbar searchMovies={fetchData}/>
       </div>
 
-      <div id="movies">
-        <MovieList moviesToDisplay={availableMovies} voted={false}/>
-      </div>
+      <DragDropContext onDragEnd={handleDragEnd}>
 
-      <div id="added-movies">
-        <MovieList moviesToDisplay={[]} voted={true}/>
-      </div>
+        <div id="movies" className="movie-container">
+          <h3 className="heading-h3">
+            {"Search Results"}
+          </h3>
+          <MovieList listId={"searchResults"}  voted={false}>
+            {getSearchResultsList()}
+          </MovieList>
+        </div>
 
+        <div id="added-movies" className="movie-container">
+          <h3 className="heading-h3">
+            {"Voted Movies"}
+          </h3>
+          <MovieList listId={"votedMovies"} voted={true}>
+            {getVotedMoviesList()}
+          </MovieList>
+        </div>
+
+      </DragDropContext>
     </div>
-  )
+  );
 }
--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,59 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import { DragDropContext, Droppable } from "react-beautiful-dnd";
+import React from 'react'
+import { Droppable } from "react-beautiful-dnd";
 import './MovieList.css'
 import _ from 'lodash'
 import Card from '../Card/Card';
 
-export default function MovieList({voted, moviesToDisplay}) {
-  const [movies, setMovies] = useState(moviesToDisplay);
-  const [listTitle] = useState(() => {
-    if (voted) {
-      return "Voted Movies";
-    } else {
-      return "Search Results";
-    }
-  })
-
-  useEffect(() => {
-    setMovies(moviesToDisplay);
-  }, [moviesToDisplay]);
-
-  const handleDragEnd = (result) => {
-    if (result.destination) {
-      let newMovies = _.cloneDeep(movies);
-      let [draggedMovie] = newMovies.splice(result.source.index, 1);
-      newMovies.splice(result.destination.index, 0, draggedMovie);
-      setMovies(newMovies);
-    }
-  };
+export default function MovieList({listId, voted, children}) {
 
   return (
-    <div className="container">
-      <h3 className="heading-h3">
-        {listTitle}
-      </h3>
-      <DragDropContext onDragEnd={handleDragEnd}>
-        <Droppable droppableId="searchResults">
-          {
-            (provided) => (
-              <div className="list" {...provided.droppableProps} ref={provided.innerRef}>
-                {
-                  _.map(movies, (movie, index) => {
-                    return (
-                      <React.Fragment key={movie.id}>
-                        <Card movie={movie} index={index} />
-                      </React.Fragment>
-                    );
-                  })
-                }
-                {
-                  provided.placeholder
-                }
-              </div>
-            )
-          }
-        </Droppable>
-      </DragDropContext>
-    </div>
+    <Droppable droppableId={listId}>
+      {
+        (provided) => (
+          <div className="list" {...provided.droppableProps} ref={provided.innerRef}>
+            {
+              _.map(children, (movie, index) => {
+                return (
+                  <React.Fragment key={movie.id}>
+                    <Card movie={movie} index={index} />
+                  </React.Fragment>
+                );
+              })
+            }
+            {
+              provided.placeholder
+            }
+          </div>
+        )
+      }
+    </Droppable>
   )
 }
