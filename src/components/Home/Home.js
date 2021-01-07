a79903f901785dd67fb819bcdf132997c8636a31
lboulanger <ludovic.boulanger@outlook.com>
2021-01-07 08:56:22 -0500

    Added Search bar functionnality

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,26 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './Home.css';
 import MovieList from '../MovieList/MovieList';
 import { fetchMovies } from '../../utils/MovieFetcher';
+import Searchbar from '../Search/Search';
 
 export default function Home() {
   const [availableMovies, setAvailableMovies] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      let movies = await fetchMovies("ram");
-      console.log(movies);
-      setAvailableMovies(movies);
-    }
+  const fetchData = async (title) => {
+    let movies = await fetchMovies(title);
+    console.log(movies);
+    setAvailableMovies(movies);
+  };
 
-    fetchData();
-  }, []);
-  
   return(
     <div className="main-grid">
       <div className="header">
         <h1 className="title">Shoppies</h1>
         <p className="main-text">Please, vote for your 5 all time favorite movies down below!</p>
+      </div>
+
+      <div id="search">
+        <Searchbar searchMovies={fetchData}/>
       </div>
 
       <div id="movies">
