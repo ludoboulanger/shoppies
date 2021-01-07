5f319f4c990097ecd101ddb26dfabf67a00f06d9
lboulanger <ludovic.boulanger@outlook.com>
2021-01-06 19:47:25 -0500

    Created the Movie Cards

---
+++ src/components/Card/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Card.css';
+
+export default function Card({title}) {
+
+    return (
+        <div class='movie-card'>
+            <p>This is a movie Card</p>
+        </div>
+    )
+}
--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,20 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './MovieList.css'
+import Card from '../Card/Card';
 import _ from 'lodash'
 
-export default function MovieList() {
+export default function MovieList({type, moviesToDisplay}) {
   const [movies] = useState([1,2,3,4,5]);
 
   return (
     <div>
-      <ul>
+      <h3 className="heading-h3">
+        {type}
+      </h3>
+      <ul className="list">
         {
           _.map(movies, (movie, index) => {
             return (
               <React.Fragment key={index}>
-                <div>
-                  Hello
-                </div>
+                <Card title={"Bob"}></Card>
               </React.Fragment>
 
             )
